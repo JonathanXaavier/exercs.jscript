56904419ede1d7087dd52ec43c8ec5e641ef37f4
JonathanXaavier <106411759+JonathanXaavier@users.noreply.github.com>
2022-07-12 21:07:46 -0300

    exerc008

---
+++ exerc008/exerc008.js
@@ -0,0 +1,23 @@
+/*
+Como saber se o numero é par ou impar?
+*/
+function parimpar(n){
+    if(n%2==0){
+        return "Par!"
+    } else {
+        return "Impar!"
+    }
+}
+ let res = parimpar(10)
+ console.log(res)
+
+ /*
+Como fazer uma conta basica de adição?
+ */
+
+function soma(n1,n2){
+    return n1 + n2
+}
+
+let resp = soma(10,5)
+console.log(resp)
