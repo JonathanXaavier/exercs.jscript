50c447ed1935de7031005eb24514920266aaacb1
JonathanXaavier <106411759+JonathanXaavier@users.noreply.github.com>
2022-07-18 21:16:48 -0300

    exerc009

--- exerc009/script.js
+++ exerc009/script.js
@@ -1,21 +1,50 @@
-function verificar(){
+function verificar() {
    var data = new Date() /* Comando para pegar a data atual */
    var ano = data.getFullYear() /* Comando para pegar o ano atual */
-   var form_ano = document.getElementById("ano") /* Ano que veio do formulário no HTML(ano de nascimento) */
-   var res = document.getElementById("resultado")
+   var fano = document.getElementById("txtano") /* Ano que veio do formulário no HTML(ano de nascimento) */
+   var res = document.querySelector("div#res")
 
-   if(Number(form_ano.value) == 0 || Number(form_ano) > ano){
+   if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert("[ERRO] Verifique os dados e tente novamente!")
    } else {
-        var fsex = document.getElementsByClassName('radsex')
-        var idade = ano - Number(form_ano.value)
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
         var genero = ''
-
-        if (fsex[0].checked){
-            genero = "Homem"
-        } else if (fsex[1].checked){
-            genero = "Mulher"
+        var img = document.createElement('img') /* Esse comando cria uma tag pra mim */
+        img.setAttribute('id','foto') /* Neste comando atribui um ID foto na tag img acima */
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                //crianca
+                img.setAttribute('src','2bb-menino.jpg')
+            }
+            else if (idade < 21 ){
+                //jovem
+                
+            }
+            else if (idade < 50){
+                //adulto
+            }
+            else {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //crianca
+            }
+            else if (idade < 21 ){
+                //jovem
+            }
+            else if (idade < 50){
+                //adulto
+            }
+            else {
+                //idoso
+            }
         }
-        res.innerHTML = `Detectado ${genero} com ${idade} anos `
-    }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectado ${genero} com ${idade} anos.`
+        res.appendChild(img)
+   } 
 }
